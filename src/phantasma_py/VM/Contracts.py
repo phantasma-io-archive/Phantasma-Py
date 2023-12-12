cdad7afe8e31317aa140988e20c3876d699c347e
João Novo <34069419+TeknoPT@users.noreply.github.com>
2023-12-12 16:01:18 +0000

    Updating Setup
    - Fix Contracts

--- src/phantasma_py/VM/Contracts.py
+++ src/phantasma_py/VM/Contracts.py
@@ -1,6 +1,6 @@
-from enum import Enum
+from enum import IntEnum
 
-class Contracts(Enum):
+class Contracts(IntEnum):
     GasContractName = "gas"
     BlockContractName = "block"
     StakeContractName = "stake"
